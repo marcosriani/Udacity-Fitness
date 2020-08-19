72343c6270bbdd6e5708cc88e8c792d9d7c369ba
Marcos Riani <marcosriani@hotmail.com>
2020-08-19 20:33:02 +0100

    Playing with react-navigator adding icons

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { Text, View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
 
 const Home = () => {
   return (
@@ -21,28 +23,46 @@ const Dashboard = () => {
   );
 };
 
-const Tab = createMaterialTopTabNavigator();
-const myTabs = () => {
+const Tabs = createMaterialBottomTabNavigator();
+
+const MyTabs = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name='Home' component={Home} />
-        <Tab.Screen name='Dashboard' component={Dashboard} />
-      </Tab.Navigator>
+      <Tabs.Navigator>
+        <Tabs.Screen
+          name='Home'
+          component={Home}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <FontAwesome name='home' size={18} color={'black'} />
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name='Dashboard'
+          component={Dashboard}
+          options={{
+            tabBarIcon: (tabInfo) => (
+              <FontAwesome name='home' size={18} color={'black'} />
+            ),
+          }}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
 };
+
+export default class App extends Component {
+  render() {
+    return <MyTabs />;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    color: 'blue',
   },
 });
-
-export default class App extends Component {
-  render() {
-    return <View style={styles.container}>{myTabs()}</View>;
-  }
-}
