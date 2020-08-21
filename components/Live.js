ca19d0398d897838e65058bcec8694fd7bf0870a
Marcos Riani <marcosriani@hotmail.com>
2020-08-21 19:47:42 +0100

    Add animation to the Live component

--- components/Live.js
+++ components/Live.js
@@ -5,6 +5,7 @@ import {
   ActivityIndicator,
   TouchableOpacity,
   StyleSheet,
+  Animated,
 } from 'react-native';
 import { Foundation } from '@expo/vector-icons';
 import { purple, white } from '../utils/colors';
@@ -17,9 +18,14 @@ class Live extends Component {
     coords: null,
     status: null,
     direction: '',
+    bounceValue: new Animated.Value(1),
   };
 
   componentDidMount() {
+    this.askPermission();
+  }
+
+  askPermission = () => {
     Permissions.getAsync(Permissions.LOCATION)
       .then(({ status }) => {
         if (status === 'granted') {
@@ -33,7 +39,7 @@ class Live extends Component {
 
         this.this.setState(() => ({ status: 'undetermined' }));
       });
-  }
+  };
 
   askPermission = () => {
     Permissions.askAsync(Permissions.LOCATION)
@@ -57,7 +63,22 @@ class Live extends Component {
       },
       ({ coords }) => {
         const newDirection = calculateDirection(coords.heading);
-        const { direction } = this.state;
+        const { direction, bounceValue } = this.state;
+
+        if (newDirection !== direction) {
+          Animated.sequence([
+            Animated.timing(bounceValue, {
+              duration: 200,
+              toValue: 1.04,
+              useNativeDriver: true,
+            }),
+            Animated.spring(bounceValue, {
+              toValue: 1,
+              friction: 4,
+              useNativeDriver: true,
+            }),
+          ]).start();
+        }
 
         this.setState(() => ({
           coords,
@@ -69,7 +90,7 @@ class Live extends Component {
   };
 
   render() {
-    const { coords, status, direction } = this.state;
+    const { coords, status, direction, bounceValue } = this.state;
 
     if (status === null) {
       return <ActivityIndicator style={{ marginTop: 30 }} />;
@@ -103,7 +124,20 @@ class Live extends Component {
       <View style={styles.container}>
         <View style={styles.directionContainer}>
           <Text style={styles.header}>You're heading</Text>
-          <Text style={styles.direction}>{direction}</Text>
+          <Animated.Text
+            style={[
+              styles.direction,
+              {
+                transform: [
+                  {
+                    scale: bounceValue,
+                  },
+                ],
+              },
+            ]}
+          >
+            {direction}
+          </Animated.Text>
         </View>
         <View style={styles.metricContainer}>
           <View style={styles.metric}>
